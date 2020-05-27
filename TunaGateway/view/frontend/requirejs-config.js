a81d2058475fb23b23698e1c399c57e3df065d52
matheus <mlessadev@gmail.com>
2020-05-27 16:57:52 -0300

    ajustes

---
+++ TunaGateway/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            "jquery_mask":  "Tuna_TunaGateway/js/jquery.mask.min",
+        }
+    }
+};
--- TunaGateway/view/frontend/web/js/view/payment/method-renderer/tuna-method.js
+++ TunaGateway/view/frontend/web/js/view/payment/method-renderer/tuna-method.js
@@ -1,9 +1,12 @@
 define(
     [
+        'jquery',
+        'Magento_Checkout/js/model/quote',
         'Magento_Checkout/js/view/payment/default'
     ],
-    function (Component) {
+    function ($, quote, Component) {
         'use strict';
+        quote.paymentMethod.subscribe(function (method) { console.log(method); }, null, 'change');
         return Component.extend({
             defaults: {
                 template: 'Tuna_TunaGateway/payment/tuna'
@@ -37,6 +40,24 @@ define(
                     };
                 });
             },
+            formatCreditCard: function () {
+                let ccNumber = jQuery("#tuna_credit_card_number")[0].value.replace(/[^0-9 ]/g, "")
+                jQuery("#tuna_credit_card_number")[0].value = this.ccFormater(ccNumber);
+            },
+            ccFormater: function (value) {
+                var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+                var matches = v.match(/\d{4,16}/g);
+                var match = matches && matches[0] || ''
+                var parts = []
+                for (let i = 0, len = match.length; i < len; i += 4) {
+                    parts.push(match.substring(i, i + 4))
+                }
+                if (parts.length) {
+                    return parts.join(' ')
+                } else {
+                    return value
+                }
+            },
             getCookie: function (cname) {
                 let name = cname + "=";
                 let decodedCookie = decodeURIComponent(document.cookie);
@@ -53,13 +74,16 @@ define(
                 return "";
             },
             placeOrder: function () {
-                let ccNumber = document.getElementById('tuna_credit_card_number').value;
-                let sessionID = this.getCookie("tuna_sessionid");
+                //let sessionID = this.getCookie("tuna_sessionid");
+                //var paymentData = quote.paymentMethod();
                 let data = {
-                    ccNumber,
-                    sessionID
+                    cardHolder: document.getElementById('creditCardHolder').value,
+                    cardNumber: document.getElementById('tuna_credit_card_number').value,
+                    cvv: document.getElementById('creditCardCode').value,
+                    expirationMonth: document.getElementById('creditCardExpirationMonth').value,
+                    expirationYear: document.getElementById('creditCardExpirationYear').value,
                 };
-                jQuery.post("https://5ec81c52155c130016a908a7.mockapi.io/Tuna/Tokenizer", data, function (returnedData) {
+                $.post("https://5ec81c52155c130016a908a7.mockapi.io/Tuna/Tokenizer", data, function (returnedData) {
                     console.log(returnedData);
                 });
             }
